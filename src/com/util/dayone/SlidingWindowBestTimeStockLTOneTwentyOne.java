2f18cc8a4521be01c7e348eb1094f43a5ef19c84
shiva <d.shiva0212@gmail.com>
2025-11-27 22:58:15 +0530

    Best Time to buy and sell stock

--- src/com/util/dayone/SlidingWindowBestTimeStockLTOneTwentyOne.java
+++ src/com/util/dayone/SlidingWindowBestTimeStockLTOneTwentyOne.java
@@ -1,6 +1,6 @@
 package com.util.dayone;
 
-public class SlidingWindowBestTimeStock {
+public class SlidingWindowBestTimeStockLTOneTwentyOne{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
